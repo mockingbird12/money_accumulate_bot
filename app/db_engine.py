df64289b35f5a90d03c1f3cfc7ab83f09614ed6d
mockingbird12 <mihazyx@yandex.ru>
2023-05-25 22:53:02 +0300

    Добавил поля дата изменений в таблицу учета активов. Изменил отношения между currency и assert Изменил схему состояний.

--- app/db_engine.py
+++ app/db_engine.py
@@ -18,6 +18,7 @@ class Assert(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(30), unique=True)
     assert_value = relationship("AccountingAssert")
+    currency_id = Column(Integer, ForeignKey("currency.id"))
 
     def __str__(self):
         # return f"{self.name} {self.saving_value[-1].value}"
@@ -33,7 +34,6 @@ class AccountingAssert(Base):
     id = Column(Integer, primary_key=True)
     value = Column(Integer)
     saving_id = Column(Integer, ForeignKey("assert.id"))
-    currency_id = Column(Integer, ForeignKey("currency.id"))
     update_time = Column(DateTime, default=datetime.now())
 
     def __str__(self):
@@ -47,7 +47,7 @@ class Currency(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
-    accounting_assert_id = relationship("AccountingAssert")
+    accounting_assert_id = relationship("Assert")
 
     def __str__(self):
         return f"{self.id} {self.name}"
@@ -71,6 +71,12 @@ class DB_driver():
         self.session.add(assert_value)
         self.session.commit()
 
+    def set_assert_currency(self, assert_name:str, currency_name:str):
+        assert_unit = self.get_asserts(assert_name)
+        assert_unit.currency_id = self.get_currency(currency_name=currency_name).id
+        self.session.add(assert_unit)
+        self.session.commit()
+
     def get_assert_value(self, assert_name: str):
         assert_unit = self.session.query(Assert).filter(Assert.name==assert_name).one()
         print(type(assert_unit.assert_value))
@@ -86,8 +92,9 @@ class DB_driver():
         else:
             return self.session.query(Assert).filter(Assert.name == assert_name).one()
 
-    def get_asserts_value(self)->[]:
-        return self.session.query(AccountingAssert).all()
+    def get_assert_currency(self, assert_obj: Assert):
+        res = self.session.query(Currency).filter(Currency.id == assert_obj.currency_id).one()
+        return res
 
     def delete_assert(self, assert_name:str)->True:
         delete_item = self.session.query(Assert).filter(Assert.name == assert_name).one()
@@ -100,20 +107,28 @@ class DB_driver():
         self.session.add(currency)
         self.session.commit()
 
-    def get_all_currency(self)->[]:
-        res = self.session.query(Currency).all()
-        return res
+    def get_currency(self, currency_name:str=None)->[]:
+        if currency_name == None:
+            return self.session.query(Currency).all()
+        else:
+            return self.session.query(Currency).filter(Currency.name == currency_name).one()
 
 
 if __name__ == '__main__':
     db_test = DB_driver()
     db_test.create_currency('USD')
     db_test.create_currency('RUB')
-    for i in db_test.get_all_currency():
+    db_test.create_assert('Cash')
+    db_test.create_assert('Portfolio')
+    db_test.set_assert_currency('Cash','USD')
+    db_test.set_assert_currency('Portfolio', 'RUB')
+    db_test.set_assert_value('Cash', 1000)
+    db_test.set_assert_value('Portfolio', 2345)
+    for i in db_test.get_currency():
         print(i.name)
     # set_saving_value('Наличные', 1000)
     # print(get_savings('Jetlend'))
-    [print(i) for i in db_test.get_asserts_value()]
-    # print(get_saving_value('Наличные'))
-    # [print(i) for i in get_savings_value()]
-
+    for assert_unit in db_test.get_asserts():
+        print(assert_unit.name)
+        print(assert_unit.assert_value[-1].value)
+        print(db_test.get_assert_currency(assert_unit))
